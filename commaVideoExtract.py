892506ef4ea8955c510859c124cd88e17dac774b
Shane <spoole@M1P.lan>
2022-12-27 02:29:11 -0600

    Program accepts target path command line argument, generates all video catalogs for each datetime available in target path, prompts user to select specific datetime or all and concats appropriate videos into single mp4 file

--- commaVideoExtract.py
+++ commaVideoExtract.py
@@ -1,34 +1,97 @@
 import os
+import sys
+from argparse import ArgumentParser
 import ffmpeg
-from scp import SCPClient
+import timeit
+# from scp import SCPClient
 
-basePath = "/Users/spoole/Desktop/rsync/realdata/"
+def catalogVids(basePath):
+    dirDict = {}
+    for dir in os.scandir(basePath):
+        try:
+            vidFile = dir.path.split('/')[-1][:20]
+            vidNum = int(dir.path.split('/')[-1][22:])
+            if len(vidFile) == 20:
+                if vidFile not in dirDict:
+                    dirDict[vidFile] = vidNum
+                elif vidNum > dirDict[vidFile]:
+                    dirDict[vidFile] = vidNum
+        except ValueError as ve:
+            # print(f"ValueError: {dir.path}\n{ve}")
+            continue
+        except Exception as e:
+            print(f"Directory Parse Error: {dir.path}\n{e}")
+    
+    for date in dirDict:
+        with open(f'{basePath}/{date}_vidList.txt', 'w', encoding='utf-8') as vidList:
+            for i in range(dirDict[date]+1):
+                vidFile = f"{basePath}/{date}--{i}/fcamera.hevc"
+                vidList.write(f"file '{vidFile}'\n")
 
-# for root, dirs, files in os.walk(basePath):
-#     for file in files:
-#         print(os.path.join(root, file))
+def checkVids(basePath):
+    dateList = []
+    for item in os.scandir(basePath):
+        if item.is_file():
+            if item.name.endswith('_vidList.txt'):
+                dateList.append(item.name)
+    match len(dateList):
+        case 0:
+            sys.exit('No video files found')
+        case 1:
+            return [dateList[0]]
+        case _ if len(dateList) > 1:
+            for idx, date in enumerate(dateList):
+                print(f'{idx + 1}. {date.split("_")[0]}')
+            for _ in range(3):
+                try:
+                    userChoice = input(f"Choose date/time of video (1-{len(dateList)}) or type 'all':\n")
+                    if userChoice.lower().strip() == 'all':
+                        return dateList
+                    else:
+                        return [dateList[int(userChoice) - 1]]
+                except ValueError as ve:
+                    print("Please enter a vaild option")
+                    continue
+                except Exception as e:
+                    sys.exit(f"Video Choice Error: {e}")
+            sys.exit("Too Many Invalid Choices, Please Try Again")
 
-dirDict = {}
-for dir in os.scandir(basePath):
+def concatVid(basePath, vidList):
+    # print(f"{basePath}/{vidList}")
+    for vid in vidList:
+        date = vid.split('_')[0]
+        try:
+            start = timeit.default_timer()
+            ffmpeg.input(f'{basePath}/{vid}', f='concat', safe=0, r=20)\
+                .output(f'{basePath}/{date}.mp4', codec='copy', map=0, vtag='hvc1')\
+                .run(capture_stdout=True, capture_stderr=True, quiet=True, overwrite_output=True)
+            end = timeit.default_timer()
+            print(f"{date} Concatenation Complete, Wall-Time {end-start:.2f} seconds")
+        except ffmpeg.Error as e:
+            sys.exit(f"ffmpg error:\nstdout: {e.stdout.decode('utf-8')}\nstderr: {e.stderr.decode('utf-8')}")
+        except Exception as e:
+            sys.exit(f"Concat/ffmpg Fatal Error: {e}")
+
+def initParser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'path',
+        help='Path to directory containing video files',
+        type=str)
+    return parser.parse_args()
+
+def main():
+    args = initParser()
+    path = args.path
     try:
-        vidFile = dir.path.split('/')[-1][:20]
-        vidNum = int(dir.path.split('/')[-1][22:])
-        if len(vidFile) == 20:
-            if vidFile not in dirDict:
-                dirDict[vidFile] = vidNum
-            elif vidNum > dirDict[vidFile]:
-                dirDict[vidFile] = vidNum
-    except ValueError as ve:
-        print(f"ValueError: {dir.path}\n{ve}")
+        catalogVids(path)
+        vidList = checkVids(path)
+        concatVid(path, vidList)
+        sys.exit('Success')
+    except KeyboardInterrupt:
+        sys.exit('\nKeyboardInterrupt')
+    except Exception as e:
+        sys.exit(f"Error: {e}")
 
-# print(dirDict)
-
-for date in dirDict:
-    with open(f'./{date}_vidList.txt', 'w', encoding='utf-8') as vidList:
-        for i in range(dirDict[date]):
-            vidFile = f"/realdata/{date}--{i}/fcamera.hevc"
-            # vidFile = os.path.abspath(vidFile)
-            vidList.write(f"file '{os.getcwd()}{vidFile}'\n")
-
-ffmpeg.input('/Users/spoole/Desktop/rsync/2022-11-28--16-43-50_vidList.txt', f='concat', safe=0, r=20)\
-    .output('./testOut.mp4', codec='copy', map=0, vtag='hvc1').run(quiet=True)
+if __name__ == '__main__':
+    main()
